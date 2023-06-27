634fe4de78e444f24db64ee5490243f1e4c3f3e6
anwar-2000 <khaledkhaouani5@gmail.com>
2023-06-27 16:29:27 +0100

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import  Image from 'next/image'
+import { footerLinks } from '@/constants'
+import Link from 'next/link'
+interface Props {}
+
+type ColumnProps = {
+    title : string,
+    links : Array<string>
+}
+
+
+const FooterColumn = ({title , links} : ColumnProps) => (
+        <div className='footer_column'>
+            <h4 className='font-semibold'>{title}</h4>
+            <ul className='flex flex-col gap-2 font-normal'>
+                    {links.map((link)=>(
+                        <Link href={'/'} key={link}>{link}</Link>
+                    ))}
+            </ul>
+
+        </div>
+)
+
+
+
+const Footer = () => {
+  return <footer className='flexStart footer'>
+        <div className='flex flex-col gap-12 w-full'>
+            <div className='flex items-start flex-col'>
+                <Image src="/logo-purple.svg" height={38} width={115} alt="Friibble" />
+                <p className='text-start font-normal text-sm mt-5 max-w-xs'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, reprehenderit atque minima quae consequuntur impedit corrupti a! In 
+                </p>
+            </div>
+
+            <div className='flex - flew-wrap gap-12'>
+                    <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
+
+                    <div className='flex-1 flex-col gap-4'>
+                    <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links}/>
+                    <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links}/>
+                    </div>
+                    
+                    <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links}/>
+                    <div className='flex-1 flex-col gap-4'>
+                    <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links}/>
+                    <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links}/>
+                    </div>
+
+                    <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links}/>
+
+
+            </div>
+        </div>
+
+        <div className='flexBetween footer__copyright'>
+            <p>@2023 Friibble . All Rights Reserved</p>
+            <p className='text-gray ml-2'>
+                <span className='text-black font-semibold'>10,230</span> projects submitted
+            </p>
+        </div>
+  </footer>
+}
+
+export default Footer
+
